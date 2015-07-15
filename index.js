0af50779155f434f601624efbba6a354ca910551
Beau Gunderson <beau@beaugunderson.com>
2015-07-15 17:49:32 -0400

    completely change the API

--- index.js
+++ index.js
@@ -7,200 +7,190 @@ var _ = require('lodash');
 
 var dictFile = path.join(__dirname, '/data/cmudict.0.7a');
 
-var activeSloppy = exports.activeSloppy = function (ws, fuzz) {
-  // active rhyming region: slice off the leading consonants
-  var lastConsonant = _.findLastIndex(ws, function (w) {
-    return w.match(/^[^AEIOU]/i);
-  });
+var RE_CONSONANT = /^[^AEIOU]/i;
+var RE_VOWEL = /^[AEIOU]/i;
 
-  lastConsonant -= Math.min(fuzz, ws.length - lastConsonant);
+// the last group of either "consonant, vowel(s)" or "vowel, "consonant(s)"
+var lastGroup = exports.lastGroup = function (ws) {
+  var re = RE_VOWEL;
 
-  return ws.slice(lastConsonant).join(' ');
+  if (RE_CONSONANT.test(ws[ws.length - 1])) {
+    re = RE_CONSONANT;
+  }
+
+  for (var i = ws.length - 1; i >= 0; i--) {
+    if (!ws[i].match(re)) {
+      break;
+    }
+  }
+
+  return ws.slice(i).join(' ');
 };
 
+// active rhyming region: slice off the leading consonants
 var active = exports.active = function (ws) {
-  // active rhyming region: slice off the leading consonants
   var firstNonConsonant = _.findIndex(ws, function (w) {
-    return !w.match(/^[^AEIOU]/i);
+    return !w.match(RE_CONSONANT);
   });
 
   return ws.slice(firstNonConsonant).join(' ');
 };
 
-exports.loadData = function (cb, dictFileDirect) {
-  var self = {};
-  var dict = {};
+var Rhyme = exports.Rhyme = function rhyme() {
+  this.dict = {};
+};
 
-  self.pronounce = function (word) {
-    return dict[word.toUpperCase()];
-  };
+Rhyme.prototype.load = function (cb, dictFileDirect) {
+  var stream = fs.createReadStream(dictFileDirect || dictFile);
 
-  self.syllables = function (word) {
-    var prose = self.pronounce(word);
-    return prose && prose[0].filter(function (ph) {
-      return ph.match(/^[AEIOU]/);
-    }).length;
-  };
-
-  self.rhyme = function (word) {
-    word = word.toUpperCase();
-
-    if (!dict[word]) {
-      return [];
-    }
-
-    var xs = dict[word].reduce(function (acc, w) {
-      acc[active(w)] = true;
-      return acc;
-    }, {});
-
-    var rhymes = [];
-
-    Object.keys(dict).forEach(function (w) {
-      if (w === word) {
-        return;
-      }
-
-      var some = dict[w].some(function (p) {
-        return xs[active(p)];
-      });
-
-      if (some) {
-        rhymes.push(w);
-      }
-    }, []);
-
-    return rhymes;
-  };
-
-  self.doRhymeSloppy = function (word1, word2, optionalFuzz) {
-    if (!optionalFuzz) {
-      optionalFuzz = 0;
-    }
-
-    word1 = word1.toUpperCase();
-    word2 = word2.toUpperCase();
-
-    var rhyme1 = dict[word1];
-    var rhyme2 = dict[word2];
-
-    // reject if rhymes not possible
-    if (!rhyme1 || !rhyme2 || rhyme1 === rhyme2) {
-      return false;
-    }
-
-    var xs = rhyme1.reduce(function (acc, w) {
-      acc[activeSloppy(w, optionalFuzz)] = true;
-      return acc;
-    }, {});
-
-    var foundRhyme = rhyme2.some(function (p) {
-      return xs[activeSloppy(p, optionalFuzz)];
-    });
-
-    return foundRhyme;
-  };
-
-  self.doRhyme = function(word1, word2) {
-    // find words in rhyming dictionary
-    word1 = word1.toUpperCase();
-    word2 = word2.toUpperCase();
-
-    var rhyme1 = dict[word1];
-    var rhyme2 = dict[word2];
-
-    // reject if rhymes not possible
-    if (!rhyme1 || !rhyme2 || rhyme1 === rhyme2) {
-      return false;
-    }
-
-    var xs = rhyme1.reduce(function (acc, w) {
-      acc[active(w)] = true;
-      return acc;
-    }, {});
-
-    var foundRhyme = rhyme2.some(function (p) {
-      return xs[active(p)];
-    });
-
-    return foundRhyme;
-  };
-
-  self.findRhymes = function(words) {
-    // see if words rhyme
-    var rhymes = [];
-
-    for (var w = 0; w < words.length - 1; w++) {
-      for (var w2 = w + 1; w2 < words.length; w2++) {
-        if (this.doRhyme(words[w], words[w2])) {
-          rhymes.push([words[w], words[w2]]);
-        }
-      }
-    }
-
-    return rhymes;
-  };
-
-  self.alliteration = function (word) {
-    word = word.toUpperCase();
-
-    if (!dict[word]) {
-      return [];
-    }
-
-    function firstSlice(ws) {
-      // active rhyming region: slice off the trailing consonants
-      for (var i = ws.length - 1; i > 0; i--) {
-        if (!ws[i].match(/^[^AEIOU]/i)) {
-          break;
-        }
-      }
-
-      ws.splice(i + 1);
-
-      return ws.join(' ');
-    }
-
-    var xs = dict[word].reduce(function (acc, w) {
-      acc[firstSlice(w)] = true;
-      return acc;
-    }, {});
-
-    var rhymes = [];
-
-    Object.keys(dict).forEach(function (w) {
-      if (w === word) {
-        return;
-      }
-
-      var some = dict[w].some(function (p) {
-        return xs[firstSlice(p)];
-      });
-
-      if (some) {
-        rhymes.push(w);
-      }
-    }, []);
-
-    return rhymes;
-  };
-
-  var s = fs.createReadStream(dictFileDirect || dictFile);
-
-  s.on('end', function () {
-    cb(self);
+  stream.on('end', function () {
+    cb();
   });
 
-  new Lazy(s).lines.map(String).forEach(function (line) {
+  var self = this;
+
+  // Lazy is probably no longer needed
+  new Lazy(stream).lines.map(String).forEach(function (line) {
     if (line.match(/^[A-Z]/i)) {
       var words = line.split(/\s+/);
-      var w = words[0].replace(/\(\d+\)$/, '');
+      var word = words[0].replace(/\(\d+\)$/, '');
 
-      if (!dict[w]) {
-        dict[w] = [];
+      if (!self.dict[word]) {
+        self.dict[word] = [];
       }
 
-      dict[w].push(words.slice(1));
+      self.dict[word].push(words.slice(1));
     }
   });
+};
+
+Rhyme.prototype.pronounce = function (word) {
+  return this.dict[word.toUpperCase()];
+};
+
+Rhyme.prototype.syllables = function (word) {
+  var pronounciations = this.pronounce(word);
+
+  return pronounciations && pronounciations[0].filter(function (phoneme) {
+    return phoneme.match(RE_VOWEL);
+  }).length;
+};
+
+Rhyme.prototype.rhyme = function (word) {
+  word = word.toUpperCase();
+
+  if (!this.dict[word]) {
+    return [];
+  }
+
+  var mapped = this.dict[word].map(active);
+  var rhymes = [];
+
+  _.each(this.dict, function (pronounciations, w) {
+    if (w === word) {
+      return;
+    }
+
+    var some = pronounciations.some(function (p) {
+      return mapped.indexOf(active(p)) !== -1;
+    });
+
+    if (some) {
+      rhymes.push(w);
+    }
+  });
+
+  return rhymes;
+};
+
+// TODO: look up what these numbers mean, e.g. EH0 vs. EH1 in instead/forehead
+function removeNumbers(pronounciation) {
+  return pronounciation.map(function (phoneme) {
+    return phoneme.replace(/[0-9]+$/g, '');
+  });
+}
+
+Rhyme.prototype.applyRhymeFn = function (fn, word1, word2) {
+  var pronounciations1 = this.dict[word1.toUpperCase()];
+  var pronounciations2 = this.dict[word2.toUpperCase()];
+
+  // reject if rhymes not possible
+  if (!pronounciations1 || !pronounciations2 ||
+      pronounciations1 === pronounciations2) {
+    return false;
+  }
+
+  pronounciations1 = pronounciations1.map(removeNumbers);
+  pronounciations2 = pronounciations2.map(removeNumbers);
+
+  var mapped = pronounciations1.map(fn);
+
+  var foundRhyme = pronounciations2.some(function (pronounciation) {
+    return mapped.indexOf(fn(pronounciation)) !== -1;
+  });
+
+  return foundRhyme;
+};
+
+Rhyme.prototype.doesLastGroupRhyme = function (word1, word2) {
+  return this.applyRhymeFn(lastGroup, word1, word2);
+};
+
+Rhyme.prototype.doesRhyme = function (word1, word2) {
+  return this.applyRhymeFn(active, word1, word2);
+};
+
+Rhyme.prototype.findRhymes = function (words) {
+  // see if words rhyme
+  var rhymes = [];
+
+  for (var w = 0; w < words.length - 1; w++) {
+    for (var w2 = w + 1; w2 < words.length; w2++) {
+      if (this.doesRhyme(words[w], words[w2])) {
+        rhymes.push([words[w], words[w2]]);
+      }
+    }
+  }
+
+  return rhymes;
+};
+
+Rhyme.prototype.alliteration = function (word) {
+  word = word.toUpperCase();
+
+  if (!this.dict[word]) {
+    return [];
+  }
+
+  function firstSlice(ws) {
+    // active rhyming region: slice off the trailing consonants
+    for (var i = ws.length - 1; i > 0; i--) {
+      if (!ws[i].match(RE_CONSONANT)) {
+        break;
+      }
+    }
+
+    ws.splice(i + 1);
+
+    return ws.join(' ');
+  }
+
+  var mapped = this.dict[word].map(firstSlice);
+  var rhymes = [];
+
+  _.each(this.dict, function (pronounciations, w) {
+    if (w === word) {
+      return;
+    }
+
+    var some = pronounciations.some(function (p) {
+      return mapped.indexOf(firstSlice(p)) !== -1;
+    });
+
+    if (some) {
+      rhymes.push(w);
+    }
+  });
+
+  return rhymes;
 };
